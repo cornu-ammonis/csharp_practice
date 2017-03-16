380198822f7e839a9493d480b833f5cb4e8a3130
Andrew Jones <andrewjones232@gmail.com>
2017-03-15 22:24:26 -0400

    IP validation - tests

--- src/app/Program.cs
+++ src/app/Program.cs
@@ -14,16 +14,30 @@ namespace ConsoleApplication
             //int[] nums2 = new int[2] {2,3};
            //Console.WriteLine(s.FindMedianSortedArraysOptimized(nums1, nums2).ToString());
             Console.WriteLine(s.NumberToWords(123456789));
-            //Console.WriteLine(s.numDigits(123).ToString());*/
+            //Console.WriteLine(s.numDigits(123).ToString());
 
             //LongestPalindromicSubstring longestPalindrome = new LongestPalindromicSubstring();
             //string s = "aab"; 
             //Console.WriteLine(longestPalindrome.LongestPalindromeOptimizeAttempt1(s));
 
-            LFUCache cache = new LFUCache(2);
-            cache.set(1, 1);
-            cache.set(2, 2);
-            Console.WriteLine(cache.get(2));
+            string s = "12345678";
+            string rev = "";
+            
+            for( int i = s.Length - 1; i >=0; i--) {
+                rev = rev + s[i];
+            }
+            Console.WriteLine(rev);*/
+
+            IPValidation ip = new IPValidation();
+            string testIPv4 = "172.16.254.1";
+            
+            Console.WriteLine(ip.isValidIPv4(testIPv4));
+            string testIPv6 = "2001:0db8:85a3:0000:0000:8a2e:0370:7334";
+            Console.WriteLine(ip.isValidIPv6(testIPv6));
+
+            
+
+            
             
         }
     }
